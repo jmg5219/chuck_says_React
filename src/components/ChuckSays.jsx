6089cb98d49200f2f943cf1996a410f99d83b2ed
Jai Gokhale <gokhale.jaidev@gmail.com>
2020-10-11 15:30:45 -0500

    implemented dropdown functionality

--- src/components/ChuckSays.jsx
+++ src/components/ChuckSays.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import "./chuckSays.css";
 
+
+
+
+
+
 //refactored as a function
 function ChuckSays(props) {
     //passing data to the component via props
-    const { userData, categories } = props;
-    
+    const { userData } = props;
+    const { categories } = props;
+
     //accessing the props
     return (
-        <div className = "container">
+        <div className="container">
+
             <p>{userData.value}</p>
+            {/* Creating a dropdown list and assigning value */}
+            <select id="selectBox">
+            {categories.map((category,index) => <option  key={index} value={category}>{category}</option>)}
+            </select>
+
         </div>
-        
-        
 
 
     );
